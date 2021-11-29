e06f0d2be3f6be2fab335716f6b483b96bf6cf55
Neyser Gomez <ngomez@credicapital.com.mx>
2021-11-29 16:57:51 -0600

    Views && Models

---
+++ src/model/state.rb
@@ -0,0 +1,24 @@
+module Model
+    class Coord < Struct.new(:row, :col)
+    end
+
+    class Food < Coord
+    end
+
+    class Snake < Struct.new(:positions)
+    end
+
+    class Grid < Struct.new(:rows, :cols)
+    end
+
+    class State < Struct.new(:snake, :food, :grid)
+    end
+
+    def self.initial_state
+        Model::State.new (
+            Model::Snake.new(),
+            Model::Food.new(),
+            Model::Grid.new()
+        )
+    end
+end
